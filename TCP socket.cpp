abd5ed8754f2ecc3114d2889013baec290a5592a
melomorte <55064565+melomorte@users.noreply.github.com>
2020-05-17 23:31:44 -0300

    Update TCP socket.cpp

--- TCP socket.cpp
+++ TCP socket.cpp
@@ -1,3 +1,6 @@
+//ATENÇÃO: é necessário adicionar a dependencia: " WS2_32.lib " - ao linker antes de compilar.
+//WARNING: it is necessary to add the dependency: " WS2_32.lib " - to the linker before compiling.
+
 #include <winSock2.h>
 #include <iostream>
 
@@ -27,14 +30,14 @@ int main()
 
     int iSend;
     char SenderBuffer[512] = "server - hey, how, hey, how, hey, how";
-    int iSendrBuffer = strlen(SenderBuffer)+1;
+    int iSenderBuffer = strlen(SenderBuffer)+1;
 
     int iRecv;
     char RecvBuffer[512];
     int iRecvBuffer = strlen(RecvBuffer)+1;
 
     //step - 1: WSAStartup
-    iWsaStartup = WSAStartup(MAKEWORD(2,2)&Winsockdata);
+    iWsaStartup = WSAStartup(MAKEWORD(2,2), &Winsockdata);
 
     if(iWsaStartup != 0)
     {
@@ -43,8 +46,8 @@ int main()
     cout << "WSAStartup - sucess..." << endl;
 
     //step - 2: fill the structure
-    TCPServerAdd.sin_family = AF_NET;
-    TCPServerAdd.sin_adddr.s = inet_addr("127.0.0.1");
+    TCPServerAdd.sin_family = AF_INET;
+    TCPServerAdd.sin_addr.s_addr = inet_addr("127.0.0.1");
     TCPServerAdd.sin_port = htons(8000);
 
     //step - 3: socket creation
@@ -59,7 +62,7 @@ int main()
     cout << "TCPServerSocket - sucess..." << endl;
 
     //step - 4: bind fun
-    iBind = bind(TCPServerSocket(SOCKADDR*)&TCPServerAdd, sizeof(TCPServerAdd));
+    iBind = bind(TCPServerSocket,(SOCKADDR*)&TCPServerAdd, sizeof(TCPServerAdd));
 
     if(iBind == SOCKET_ERROR)
     {
@@ -86,7 +89,135 @@ int main()
     cout << "AcceptSocket - sucess..." << endl;
 
     //step - 7: send data to client
-    iSend = send(sAcceptSocket, SenderBuffer, iSenderBuffer, 0)
+    iSend = send(sAcceptSocket, SenderBuffer, iSenderBuffer, 0);
+
+    if(iSend == SOCKET_ERROR)
+    {
+        cout << "Send - failed: " << endl;
+    }
+    cout << "Send - sucess..." << endl;
+
+    //step - 8: Recv data from client
+    iRecv = recv(sAcceptSocket,RecvBuffer, iRecvBuffer, 0);
+
+    if(iRecv == SOCKET_ERROR)
+    {
+        cout << "Recv - failed: " << endl;
+    }
+    cout << "Recv - sucess..." << endl;
+
+    //step - 9: Close Socket
+    iCloseSocket = closesocket(TCPServerSocket);
+
+    if(iCloseSocket == SOCKET_ERROR)
+    {
+        cout << "Close Socket - failed: " << endl;
+    }
+    cout << "Close Socket - sucess..." << endl;
+
+    //step - 10: Cleanup
+    iWsaCleanup = WSACleanup();
+
+    if(iWsaCleanup == SOCKET_ERROR)
+    {
+        cout << "WsaCleanup - failed: " << endl;
+    }
+    cout << "WsaCleanup - sucess..." << endl;
+
+
+    system("pause");
+    return 0;
+}
+
+#include <winSock2.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "......TCP SERVER......" << endl;
+    cout << endl;
+
+    //local variable
+    WSADATA Winsockdata;
+    int iWsaStartup;
+    int iWsaCleanup;
+
+    SOCKET TCPServerSocket;
+    int iCloseSocket;
+
+    struct sockaddr_in TCPServerAdd;
+    struct sockaddr_in TCPClientAdd;
+    int iTCPClient = sizeof(TCPClientAdd);
+
+    int iBind;
+    int iListen;
+
+    SOCKET  sAcceptSocket;
+
+    int iSend;
+    char SenderBuffer[512] = "server - hey, how, hey, how, hey, how";
+    int iSenderBuffer = strlen(SenderBuffer)+1;
+
+    int iRecv;
+    char RecvBuffer[512];
+    int iRecvBuffer = strlen(RecvBuffer)+1;
+
+    //step - 1: WSAStartup
+    iWsaStartup = WSAStartup(MAKEWORD(2,2), &Winsockdata);
+
+    if(iWsaStartup != 0)
+    {
+        cout << "WSAStartup - failed: " << endl;
+    }
+    cout << "WSAStartup - sucess..." << endl;
+
+    //step - 2: fill the structure
+    TCPServerAdd.sin_family = AF_INET;
+    TCPServerAdd.sin_addr.s_addr = inet_addr("127.0.0.1");
+    TCPServerAdd.sin_port = htons(8000);
+
+    //step - 3: socket creation
+    TCPServerSocket = socket(AF_INET,
+                             SOCK_STREAM,
+                             IPPROTO_TCP);
+
+    if(TCPServerSocket == INVALID_SOCKET)
+    {
+        cout << "TCPServerSocket - failed: " << endl;
+    }
+    cout << "TCPServerSocket - sucess..." << endl;
+
+    //step - 4: bind fun
+    iBind = bind(TCPServerSocket,(SOCKADDR*)&TCPServerAdd, sizeof(TCPServerAdd));
+
+    if(iBind == SOCKET_ERROR)
+    {
+        cout << "bind - failed: " << endl;
+    }
+    cout << "bind - sucess..." << endl;
+
+    //step - 5: listen fun
+    iListen = listen(TCPServerSocket,2);
+
+    if(iListen == SOCKET_ERROR)
+    {
+        cout << "listen - failed: " << endl;
+    }
+    cout << "listen - sucess..." << endl;
+
+    //step - 6: accept
+    sAcceptSocket = accept(TCPServerSocket, (SOCKADDR*)&TCPClientAdd, &iTCPClient);
+
+    if(sAcceptSocket == INVALID_SOCKET)
+    {
+        cout << "AcceptSocket - failed: " << endl;
+    }
+    cout << "AcceptSocket - sucess..." << endl;
+
+    //step - 7: send data to client
+    iSend = send(sAcceptSocket, SenderBuffer, iSenderBuffer, 0);
 
     if(iSend == SOCKET_ERROR)
     {
